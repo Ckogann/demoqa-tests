a3fb2de3a28242ba721cccccd123319c5315d575
Ckogann <ionov.kos@gmail.com>
2021-12-19 22:20:55 +0300

    added selenide tests

---
+++ src/test/java/ru/lamoda/TextBoxTests.java
@@ -0,0 +1,33 @@
+package ru.lamoda;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+//    @BeforeAll
+//    static void beforeAll() {
+//        Configuration.browserSize = "1920x1080";
+//    }
+
+    @Test
+    void successTest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("email@gmail.com");
+        $("#currentAddress").setValue("address");
+        $("#permanentAddress").setValue("another address");
+        $("#submit").scrollTo().click();
+
+        $("#output").shouldBe(visible);
+
+        $("#name").shouldHave(text("Alex"));
+        $("#email").shouldHave(text("email@gmail.com"));
+        $("#output #currentAddress").shouldHave(text("address"));
+        $("#output").$("#permanentAddress").shouldHave(text("another address"));
+    }
+}
