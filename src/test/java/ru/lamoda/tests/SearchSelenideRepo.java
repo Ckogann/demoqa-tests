6a61ba949d25134a5114a8209e91354e3cb7c556
Ckogann <ionov.kos@gmail.com>
2021-12-24 08:48:57 +0300

    Added test for search Github repo

---
+++ src/test/java/ru/lamoda/tests/SearchSelenideRepo.java
@@ -0,0 +1,24 @@
+package ru.lamoda.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchSelenideRepo {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void shouldSelenideRepositoryInGithub() {
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("h1").shouldHave(text("selenide / selenide"));
+    }
+}
